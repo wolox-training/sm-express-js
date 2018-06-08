ed4e23b0810bd81e14a10f9cad34e00ed3f12b12
Sebastian Murúa <sebastian.murua@wolox.cl>
2018-06-08 14:32:42 -0400

    Refactor to log errors when the evaluations are not supported.

--- app/helper/validator.js
+++ app/helper/validator.js
@@ -1,11 +1,19 @@
+const logger = require('../logger');
+
 const knownEvaluations = {
   required: (value, condition) => (condition ? !!value : true),
   minLength: (value, condition) => typeof value === 'string' && value.length >= condition,
   woloxEmail: (value, condition) => (condition ? /^([a-zA-Z0-9]|\.)+@wolox\.(cl|com\.ar)$/.test(value) : true)
 };
 
-const fulfillCondition = (object = {}, key, [evaluation, condition]) =>
-  evaluation in knownEvaluations ? knownEvaluations[evaluation](object[key], condition) : true;
+const fulfillCondition = (object = {}, key, [evaluation, condition]) => {
+  if (evaluation in knownEvaluations) {
+    return knownEvaluations[evaluation](object[key], condition);
+  }
+
+  logger.warn(`The evaluation '${evaluation}' is not supported. Missing implementation? Typo?`);
+  return true;
+};
 
 module.exports = (object, validations) => {
   const errors = [];
